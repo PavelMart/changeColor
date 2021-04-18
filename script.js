6e473730e85ba059d62998b72a8f356a6ac5419e
Pavel Martakov <martackovp@yandex.ru>
2021-04-18 23:24:27 +0300

    Слегка переделал задание

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const body = document.querySelector( 'body' ),
     h1 = document.querySelector( 'h1' ),
-    btn = document.querySelector( 'button' );
+    p = document.querySelector( 'p' );
 
 const changeBgColor = {
     red: 0,
@@ -19,14 +19,41 @@ const changeBgColor = {
         this.blue = Math.ceil( Math.random() * 255 );
     },
     changeDecToHex: function() {
+        if (this.red < 16) {
+            this.red = '0' + this.red.toString(16); 
+        } else {
+            this.red = this.red.toString(16); 
+        }
 
-        this.red = this.red.toString(16); 
-        this.green = this.green.toString(16); 
-        this.blue = this.blue.toString(16); 
+        if (this.green < 16) {
+            this.green = '0' + this.green.toString(16);
+        } else {
+            this.green = this.green.toString(16);
+        }
 
-        this.oppositeRed = this.oppositeRed.toString(16); 
-        this.oppositeGreen = this.oppositeGreen.toString(16); 
-        this.oppositeBlue = this.oppositeBlue.toString(16); 
+        if (this.blue < 16) {
+            this.blue = '0' + this.blue.toString(16); 
+        } else {
+            this.blue = this.blue.toString(16); 
+        }
+
+        if (this.oppositeRed < 16) {
+            this.oppositeRed = '0' + this.oppositeRed.toString(16); 
+        } else {
+            this.oppositeRed = this.oppositeRed.toString(16); 
+        }
+
+        if (this.oppositeGreen < 16) {
+            this.oppositeGreen = '0' + this.oppositeGreen.toString(16); 
+        } else {
+            this.oppositeGreen = this.oppositeGreen.toString(16); 
+        }
+
+        if (this.oppositeBlue < 16) {
+            this.oppositeBlue = '0' + this.oppositeBlue.toString(16); 
+        } else {
+            this.oppositeBlue = this.oppositeBlue.toString(16); 
+        }
     },
     createNewColor: function() {
         this.newColor = '#' + [this.red, this.green, this.blue].join('');
@@ -43,12 +70,9 @@ const changeBgColor = {
         this.createNewColor();
         this.createOppositeColor();
 
-        console.log('newColor: ', this.newColor);
-        console.log('oppositeColor: ', this.oppositeColor);
-
-
         h1.textContent = this.newColor.toUpperCase();
         h1.style.color = this.oppositeColor;
+        p.style.color = this.oppositeColor;
         body.style.backgroundColor = this.newColor;
     },
     getOppositeNumber: function() {
@@ -73,4 +97,4 @@ const changeBgColor = {
     },
 };
 
-btn.addEventListener('click', changeBgColor.start.bind(changeBgColor));
+body.addEventListener('dblclick', changeBgColor.start.bind(changeBgColor));
